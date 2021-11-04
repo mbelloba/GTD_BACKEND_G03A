884a0ae1611e3a2e9b04cfb5330a904e4ba9b3b5
alvaroartidiello <92579410+alvaroartidiello@users.noreply.github.com>
2021-11-04 10:15:00 +0100

    javadoc userService

--- src/main/java/com/capgemini/service/UserService.java
+++ src/main/java/com/capgemini/service/UserService.java
@@ -16,23 +16,49 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 	
+	/**
+	 * 
+	 * @param id
+	 * @return user by id
+	 */
+	
 	public Optional<User> get(Long id) {
 		return repository.findById(id);
 	}
+	
+	/**
+	 * 
+	 * @return all users in database
+	 */
 
 	public List<User> list() {
 		List<User> users = (List<User>) repository.findAll(); 
 		return users;
 	}
+	
+	/**
+	 * 
+	 * @param user
+	 * @return Create user in database
+	 */
 
 	public User create(User user) {
 		return repository.save(user);
 	}
 
+	/**
+	 * 
+	 * @param id, delete by ID
+	 */
 	public void deleteById (Long id) {
 		repository.deleteById(id);
 	}
 	
+	/**
+	 * 
+	 * @param user
+	 * @return true if user is validated
+	 */
 	public boolean validateUser (User user) {
 
         List<User> users = (List<User>) repository.findAll();
