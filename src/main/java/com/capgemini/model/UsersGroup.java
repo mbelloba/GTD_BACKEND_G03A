17fd428acfe94226b66b7548eded43673289a9b8
alvaroartidiello <92579410+alvaroartidiello@users.noreply.github.com>
2021-11-04 13:55:00 +0100

    CLase Grupos Usuarios

--- src/main/java/com/capgemini/model/UsersGroup.java
+++ src/main/java/com/capgemini/model/UsersGroup.java
@@ -1,6 +1,7 @@
 package com.capgemini.model;
 
 import java.sql.Date;
+import java.util.List;
 
 public class UsersGroup {
 	
@@ -8,5 +9,6 @@ public class UsersGroup {
 	String descripction;
 	User admin;
 	Date creationDate;
+	List<User> users;
 
 }
