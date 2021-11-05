cff7666e9ef9c078448ba6dd165055234aa8ad6d
alvaroartidiello <92579410+alvaroartidiello@users.noreply.github.com>
2021-11-05 10:16:30 +0100

    categoryDTO y taskDTO

---
+++ src/main/java/com/capgemini/dto/CategoryDTO.java
@@ -0,0 +1,15 @@
+package com.capgemini.dto;
+
+import java.util.List;
+
+import com.capgemini.model.Task;
+import com.capgemini.model.User;
+
+public class CategoryDTO {
+	public Long id;
+	public String name;
+	public User user;
+	public List<Task> tasks;
+	
+
+}
